6193b26a2a35b07dfdc8dad9d8283df343bf8998
Maicon Lima dos santos <maiconlimadossantos8@gmail.com>
2026-05-11 20:17:23 -0300

    11/05/2026

--- Herança/Exercicios3_POO_Herança.java
+++ Herança/Exercicios3_POO_Herança.java
@@ -4,13 +4,20 @@ import Herança.Cachorro;
 import Herança.Gato;
 
 public class Exercicios3_POO_Herança {
-    Animal animal = new Animal("turca", "vira-lata");
-    Cachorro meucao = new Cachorro("Rex", "Labrador");
-    meucao.latir();
-    meucao.caminhar();
-    Gato meugato = new Gato("Mia", "Siamês", "Cinza");
-    meugato.miar();
-    meugato.caminhar();
-    
+    public static void main(String[] args) {
+        // Exercício 1
+        Cachorro cachorro = new Cachorro("Rex", 5, "Labrador");
+        Gato gato = new Gato("Mia", 3, "Siamês");
+        
+        System.out.println(cachorro.exibirDados());
+        System.out.println(gato.exibirDados());
+        
+        // Exercício 2
+        Normal ingressoNormal = new Normal(50.0);
+        Vip ingressoVip = new Vip(50.0, 20.0);
+        
+        ingressoNormal.imprimeTipo();
+        System.out.println("Valor do Ingresso VIP: " + ingressoVip.getValorVip());
+    }
     
 }
